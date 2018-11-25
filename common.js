5a9d8c6d02ee01075f897ecc2bf2f2823c97ad4a
HolyPepperoni <canapini.giacomo@gmail.com>
2018-11-25 15:19:51 +0100

    Commenti nuovi

--- common.js
+++ common.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-  var animationEnd = 'animationend oAnimationEnd mozAnimationEnd webkitAnimationEnd';
-  $('.start').animate({
+  var animationEnd = 'animationend oAnimationEnd mozAnimationEnd webkitAnimationEnd'; //controlla se l'animazione è finita
+  $('.start').animate({ //animazione della schermata iniziale
     width: "100%",
     opacity: 1
   }, 1000);
 
-  $('.starth1').addClass('animated fadeInDown').one(animationEnd,function() {
-    $(this).removeClass('animated fadeInDown');
-    $(this).css('animation-iteration-count', '3');
-    $(this).addClass('animated pulse');
+  $('.starth1').addClass('animated fadeInDown').one(animationEnd,function() { //prima animazione del titolo
+    $(this).removeClass('animated fadeInDown'); //rimuove la classe contenente la prima animazione tramite il callback
+    $(this).css('animation-iteration-count', '3'); //decide quante volte fare la seconda animazione
+    $(this).addClass('animated pulse'); //seconda animazione del titolo
   });
-  $('.startspan').addClass('animated fadeInLeft');
+  $('.startspan').addClass('animated fadeInLeft'); //animazione della scritta "Initializing"
 
 });
 
