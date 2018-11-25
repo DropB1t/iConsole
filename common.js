55869105f93cda4ffc82f86ea15b7bf3b52561c7
Yuriy Rymarchuk <yura.ita.com@gmail.com>
2018-11-25 21:46:17 +0100

    Add Button To explain box

--- common.js
+++ common.js
@@ -14,24 +14,25 @@ $(document).ready(function() {
     $(this).removeClass('animated fadeInDown'); //rimuove la classe contenente la prima animazione tramite il callback
     $(this).css('animation-iteration-count', '3'); //decide quante volte fare la seconda animazione
     $(this).addClass('animated pulse').one(animationEnd,function () {
-      $(".start").addClass('animated bounceOutDown').one(animationEnd,console())
+      $(".start").addClass('animated bounceOutDown').one(animationEnd,console());
     }); //seconda animazione del titolo
   });
    //animazione della scritta "Initializing"
   function console(){
       $(".console").css('display', 'inline-block');
       $(".console").addClass('animated fadeInDown').one(animationEnd, function() {
+        $(".start").css('display', 'none');
         $(this).removeClass('animated fadeInDown');
       });
 
       var typed = new Typed('#typed',{
-        strings: ["Hello, my name is [iConsole]. I'm honored to meet you!^1000","I was created with a programming language called JavaScript",
+        strings: ["Hello, my name is [iConsole]. I'm honored to meet you!^1000","I was created with a programming language called JavaScript^1000",
                   "What?^1200 You don't know what is JavaScript?^800  ","Let me explain you very briefly..."],
         startDelay: 2500,
         cursorChar: "|",
         typeSpeed: 30,
         onComplete: function () {
-          $(".typed-cursor").hide(),
+          // $(".typed-cursor").hide(),
           explain()
         }
       });
@@ -41,6 +42,13 @@ $(document).ready(function() {
     $(".explainJS").css('display', 'inline-block');
     $(".explainJS").addClass('animated bounceInLeft').one(animationEnd, function() {
       $(this).removeClass('animated bounceInLeft');
+    });
+    $("#explain-b").click( function() {
+      $(".explainJS").css('animation-delay', '0');
+      $(".explainJS").addClass('animated bounceOutLeft fast').one(animationEnd,function () {
+        $(this).removeClass('animated bounceOutLeft');
+        $(this).css('display', 'none');
+      });
     });
   }
 
