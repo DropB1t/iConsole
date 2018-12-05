6181328a6ddb43713159fd1fc249b9bd2a95d8be
Yuriy Rymarchuk <yura.ita.com@gmail.com>
2018-12-05 23:21:54 +0100

    Button Start/ New Css Styling

--- js/common.js
+++ js/common.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
-  const animationEnd = 'animationend oAnimationEnd mozAnimationEnd webkitAnimationEnd'; //check if the animation is over
+  var animationEnd = (function(el) {
+    var animations = {
+      animation: 'animationend',
+      OAnimation: 'oAnimationEnd',
+      MozAnimation: 'mozAnimationEnd',
+      WebkitAnimation: 'webkitAnimationEnd',
+    };
+
+    for (var t in animations) {
+      if (el.style[t] !== undefined) {
+        return animations[t];
+      }
+    }
+  })(document.createElement('div')); //check if the animation is over
+
+
+  $(".start").css('display', 'none');
   $(".console").css('display', 'none');
   $(".explainJS").css('display', 'none');
 
@@ -8,17 +24,24 @@ $(document).ready(function() {
     opacity: 1
   }, 1000);
 
-  $('.startspan').addClass('animated fadeInLeft');
-  $('.starth1').addClass('animated fadeInDown').one(animationEnd,function() { //first title animation
-    $(this).removeClass('animated fadeInDown'); //remove the class of the first animation using the callback
-    $(this).css('animation-iteration-count', '3'); //decide how many times the text will be animated
-    $(this).addClass('animated pulse').one(animationEnd,function () {
-      $(".start").addClass('animated bounceOutDown').one(animationEnd,console()); //second title animation
+  $("#btn-start").click(function() {
+    $("#btn-start").animate({ //space of the first animation
+      opacity: 0
+    },1200,function(){$("#btn-start").css('display', 'none');});
+
+    $(".start").css('display', 'block');
+    $('.startspan').addClass('animated fadeInLeft');
+    $('.starth1').addClass('animated fadeInDown').one(animationEnd,function() { //first title animation
+      $(this).removeClass('animated fadeInDown'); //remove the class of the first animation using the callback
+      $(this).css('animation-iteration-count', '3'); //decide how many times the text will be animated
+      $(this).addClass('animated pulse').one(animationEnd,function () {
+        $(".start").addClass('animated fadeOutDown').one(animationEnd,console()); //second title animation
+      });
     });
   });
 
   function console(){
-      $(".console").css('display', 'inline-block');
+      $(".console").css('display', 'block');
       $(".console").addClass('animated fadeInDown').one(animationEnd, function() {
         $(".start").css('display', 'none');
         $(this).removeClass('animated fadeInDown');
@@ -32,7 +55,7 @@ $(document).ready(function() {
         typeSpeed: 30,
         onComplete: function () {
           // $(".typed-cursor").hide(),
-          $(".explainJS").css('display', 'inline-block');
+          $(".explainJS").css('display', 'block');
           $(".explainJS").addClass('animated bounceInLeft').one(animationEnd, function() {
             $(this).removeClass('animated bounceInLeft');
           });
@@ -53,10 +76,10 @@ $(document).ready(function() {
   //"Initializing" animation
   function animations(){
     var typed2 = new Typed('#typed', {
-      strings: ["Now i'm going to show you some funny things that you can do using JavaScript...^800","Something like this..."],
+      strings: ["Now I'm going to show you some funny things that you can do using JavaScript...^800","Something like this..."],
       typeSpeed: 30,
       cursorChar: "|",
-      startDelay: 2000,
+      startDelay: 1000,
       onComplete: function() {}
     });
   }
